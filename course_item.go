098d6b2f16b955149031b70d4cb6fcfaaaa1ed39
Pasu Sangiemsin <opxz7148@gmail.com>
2025-12-23 16:29:38 +0700

    feat(refactor): Embeded model intead

--- course_item.go
+++ course_item.go
@@ -4,12 +4,13 @@ type CourseItem struct {
 	Name           string `json:"name"`
 	Section        string `json:"section"`
 	ClassRoomId    string `json:"id"`
-	CoursePostList *CoursePostListModel
+	*CoursePostListModel
 }
 
 func (ci *CourseItem) FilterValue() string { return ci.Name }
 func (ci *CourseItem) Title() string       { return ci.Name }
 func (ci *CourseItem) Description() string { return ci.Section }
 func (ci *CourseItem) InitializeCoursePosts() {
-	ci.CoursePostList = NewCoursePostListModel()
+	ci.CoursePostListModel = NewCoursePostListModel()
 }
+
